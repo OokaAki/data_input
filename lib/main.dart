2e33926e7602a92fda65d687c2e6bc43269c9309
OokaAki <aki.ooka@psp.co.jp>
2022-09-23 19:17:56 +0900

    データ入力画面作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:screen_transition/next_page.dart';
 
@@ -30,71 +31,253 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // ピッカーで選択された項目をTextFormFieldに表示するためのcontroller
+  final TextEditingController _controller = TextEditingController();
   // TextFormFieldに入力された文字列
-  String inputText = '';
+  String _name = '';
+  // ラジオボタンで選択された性別
+  Gender _gender = Gender.none;
+  // ピッカーで選択された地域
+  String _region = '';
+  // ピッカーの選択肢として表示する項目のList
+  final List<String> _regionNames = <String>[
+    '北海道',
+    '東北',
+    '関東',
+    '中部',
+    '近畿',
+    '中国・四国',
+    '九州',
+  ];
 
   void _setText(String value) {
-    // 入力された文字列をinputTextに代入
+    // 入力された名前を_nameに代入
     setState(() {
-      inputText = value;
+      _name = value;
     });
+  }
+
+  void _setGender(Gender selectedGender) {
+    // 選択された性別のenumをgenderに代入
+    setState(() {
+      _gender = selectedGender;
+    });
+  }
+
+  void _setRegion(int selectedRegion) {
+    // 選択された地域をgenderに代入
+    setState(() {
+      _region = _regionNames[selectedRegion];
+    });
+    // 選択肢た出身地のテキストをTextFormFieldに表示させる
+    _controller.value = TextEditingValue(text: _region);
   }
 
   // 画面描画を書いているところ
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // ここで背景色を指定
-      backgroundColor: Colors.pink[100],
-
       // 一番上のタイトルが表示されているところ
       appBar: AppBar(
         title: Text(widget.title),
       ),
 
       // 画面のメインの部分
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // 画面上半分の
-            Expanded(
-              flex: 1,
-              // 真ん中に位置させる
-              child: Align(
-                alignment: Alignment.center,
-                // 入力フォーム
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    // 何も入力されていない時に表示するヒントテキスト
-                    hintText: 'テキストを入力',
-                  ),
-                  // 入力される文字が変わるごとに関数_setTextを実行する
-                  onChanged: (value) => _setText(value),
-                ),
-              ),
-            ),
-            // 画面下半分の
-            Expanded(
-              flex: 1,
-              // 真ん中に位置させる
-              child: Align(
-                alignment: Alignment.center,
-                // 枠線があるボタン
-                child: OutlinedButton(
-                  // 中の文字
-                  child: const Text('次へ'),
-                  // ボタンがタップされた時に画面遷移する
-                  onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                    // 遷移先の画面
-                    return NextPage(inputText);
-                  })),
-                ),
-              ),
-            ),
-          ],
+      body: Align(
+        // 画面の真ん中に位置させる
+        alignment: Alignment.center,
+        // 画面に収まりきらない時にスクロールできるようにする
+        child: SingleChildScrollView(
+          // Widgetを縦に並べる
+          child: Column(
+            children: <Widget>[
+              // 氏名
+              nameInputForm(),
+              // 性別
+              genderInputForm(),
+              // 出身地
+              regionInputForm(),
+              // 次へボタン
+              nextButton(),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  // それぞれの内容を以下で定義
+  Widget nameInputForm() {
+    return Column(
+      children: [
+        const Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              '氏名',
+              style: TextStyle(fontSize: 18.0),
+            )),
+        // 子Widgetの周りに余白を作る
+        Padding(
+          // 余白の幅やどこに付けるかを指定
+          padding: const EdgeInsets.only(bottom: 25.0),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              // 何も入力されていない時に表示するヒントテキスト
+              hintText: '山田　太郎',
+            ),
+            // 入力される文字が変わるごとに関数_setTextを実行する
+            onChanged: (value) => _setText(value),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget genderInputForm() {
+    return Column(
+      children: [
+        const Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              '性別',
+              style: TextStyle(fontSize: 18.0),
+            )),
+        // 子Widgetの周りに余白を作る
+        Padding(
+          // 余白の幅やどこに付けるかを指定
+          padding: const EdgeInsets.only(bottom: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Radio(
+                value: Gender.man,
+                groupValue: _gender,
+                onChanged: (Gender? value) {
+                  if (value != null) {
+                    _setGender(value);
+                  }
+                },
+              ),
+              const Text('男'),
+              Radio(
+                value: Gender.woman,
+                groupValue: _gender,
+                onChanged: (Gender? value) {
+                  if (value != null) {
+                    _setGender(value);
+                  }
+                },
+              ),
+              const Text('女'),
+              Radio(
+                value: Gender.other,
+                groupValue: _gender,
+                onChanged: (Gender? value) {
+                  if (value != null) {
+                    _setGender(value);
+                  }
+                },
+              ),
+              const Text('その他'),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget regionInputForm() {
+    return Column(
+      children: [
+        const Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              '出身地',
+              style: TextStyle(fontSize: 18.0),
+            )),
+        // 子Widgetの周りに余白を作る
+        Padding(
+          // 余白の幅やどこに付けるかを指定
+          padding: const EdgeInsets.only(bottom: 25.0),
+          child: TextFormField(
+            // ピッカーで選択した地域をTextFormFieldに表示させるためのcontrollerを設定
+            controller: _controller,
+            decoration: const InputDecoration(
+              // 何も入力されていない時に表示するヒントテキスト
+              hintText: '関東',
+            ),
+            onTap: () {
+              // タップした時にキーボードが出ないようにする
+              FocusScope.of(context).requestFocus(FocusNode());
+              // ピッカー（_showDialogはさらに下で定義）
+              _showDialog(
+                CupertinoPicker(
+                  itemExtent: 32.0,
+                  // ピッカーの値が変化するたびに_setRegionを実行
+                  onSelectedItemChanged: (int selectedItem) {
+                    _setRegion(selectedItem);
+                  },
+                  // ピッカーの項目を設定
+                  children: List<Widget>.generate(_regionNames.length, (int index) {
+                    return Center(
+                      child: Text(
+                        _regionNames[index],
+                      ),
+                    );
+                  }),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget nextButton() {
+    // 子Widgetの周りに余白を作る
+    return Padding(
+      // 余白の幅やどこに付けるかを指定
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: OutlinedButton(
+        // 中の文字
+        child: const Text('次へ'),
+        // ボタンがタップされた時に画面遷移する
+        onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+          // 遷移先の画面
+          return NextPage(_name, _gender, _region);
+        })),
+      ),
+    );
+  }
+
+  void _showDialog(Widget child) {
+    showCupertinoModalPopup<void>(
+        context: context,
+        builder: (BuildContext context) => Container(
+              height: 216,
+              // 余白の幅やどこに付けるかを指定
+              padding: const EdgeInsets.only(top: 6.0),
+              // Widgetの内側に余白を作る
+              // The Bottom margin is provided to align the popup above the system navigation bar.
+              margin: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              // ピッカーの背景色を指定
+              color: Colors.white70,
+              // 子Widgetがスマホ画面からはみ出さないようにする
+              child: SafeArea(
+                // 上部だけSafeAreaを無効にする
+                top: false,
+                child: child,
+              ),
+            ));
+  }
+}
+
+enum Gender {
+  man,
+  woman,
+  other,
+  none,
 }
--- lib/next_page.dart
+++ lib/next_page.dart
@@ -1,31 +1,54 @@
 import 'package:flutter/material.dart';
 
+import 'main.dart';
+
 class NextPage extends StatelessWidget {
-  final String inputText;
-  const NextPage(this.inputText, {Key? key}) : super(key: key);
+  final String name;
+  final Gender gender;
+  final String region;
+  const NextPage(this.name, this.gender, this.region, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // ここで背景色を指定
-      backgroundColor: Colors.pink[100],
-
       // 一番上のタイトルが表示されているところ
       appBar: AppBar(
         title: const Text('Next Page'),
       ),
 
       // 画面のメインの部分
-      body: Align(
-        alignment: Alignment.center,
-        child: Text(
-          // 渡した値を表示する
-          inputText,
-          // テキストのスタイルを指定できる
-          style: const TextStyle(
-            // 大きさを20に指定
-            fontSize: 20,
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              // 渡した値を表示する
+              name,
+              // テキストのスタイルを指定できる
+              style: const TextStyle(
+                // 大きさを20に指定
+                fontSize: 20,
+              ),
+            ),
+            Text(
+              // 渡した値を表示する
+              gender.toString(),
+              // テキストのスタイルを指定できる
+              style: const TextStyle(
+                // 大きさを20に指定
+                fontSize: 20,
+              ),
+            ),
+            Text(
+              // 渡した値を表示する
+              region,
+              // テキストのスタイルを指定できる
+              style: const TextStyle(
+                // 大きさを20に指定
+                fontSize: 20,
+              ),
+            ),
+          ],
         ),
       ),
     );
