cefc8dfb36f1cc71cd5dabe076cd938d49c8fd30
OokaAki <aki.ooka@psp.co.jp>
2022-09-26 13:45:41 +0900

    アプリ名変更2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:screen_transition/next_page.dart';
+import 'package:data_input/next_page.dart';
 
 void main() {
   runApp(const MyApp());
